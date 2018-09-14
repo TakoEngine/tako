ce735baa0408ea9b964b9f6ec3906be815cf6ebb
Kevin Balz <kevin.balz@web.de>
2018-09-14 21:47:51 +0200

    Bitmap manipulation

--- include/Bitmap.hpp
+++ include/Bitmap.hpp
@@ -10,9 +10,13 @@ namespace tako
         const I32 width, height;
 
         Bitmap(I32 w, I32 h);
-        Color GetPixel(I32 x, I32 y);
+        Color GetPixel(I32 x, I32 y) const;
         void SetPixel(I32 x, I32 y, Color c);
-        Color* GetData();
+        Color* GetData() const;
+
+        void Clear(Color c);
+        void FillRect(I32 x, I32 y, I32 w, I32 h, Color c);
+        void DrawBitmap(I32 x, I32 y, const Bitmap& bitmap);
     private:
         std::unique_ptr<Color[]> m_data;
     };
--- src/Bitmap.cpp
+++ src/Bitmap.cpp
@@ -10,18 +10,52 @@ namespace tako
         ASSERT(w >= 0 && h >= 0);
     }
 
-    Color Bitmap::GetPixel(I32 x, I32 y)
+    Color Bitmap::GetPixel(I32 x, I32 y) const
     {
         return m_data[y * width + x];
     }
 
     void Bitmap::SetPixel(I32 x, I32 y, Color c)
     {
+        if (x < 0 || x >= width || y < 0 || y >= height) return;
         m_data[y * width + x] = c;
     }
 
-    Color* Bitmap::GetData()
+    Color* Bitmap::GetData() const
     {
         return m_data.get();
+    }
+
+    void Bitmap::Clear(Color c)
+    {
+        for (I32 x = 0; x < width; x++)
+        {
+            for (I32 y = 0; y < height; y++)
+            {
+                m_data[y * width + x] = c;
+            }
+        }
+    }
+
+    void Bitmap::FillRect(I32 x, I32 y, I32 w, I32 h, Color c)
+    {
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                SetPixel(x + i, y + j, c);
+            }
+        }
+    }
+
+    void Bitmap::DrawBitmap(I32 x, I32 y, const Bitmap & bitmap)
+    {
+        for (int i = 0; i < bitmap.width; i++)
+        {
+            for (int j = 0; j < bitmap.height; j++)
+            {
+                SetPixel(x + i, y + j, bitmap.GetPixel(i, j));
+            }
+        }
     }
 }
