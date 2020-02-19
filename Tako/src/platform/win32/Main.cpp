dff1deb4b22fa0df32105fbcd42c196c15c3221d
Kevin Balz <kevin.balz@web.de>
2020-02-19 21:07:22 +0100

    cleanup

--- Tako/src/platform/win32/Main.cpp
+++ Tako/src/platform/win32/Main.cpp
@@ -15,68 +15,14 @@ struct Velocity
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
 {
-#ifdef NDEBUG
+#ifndef NDEBUG
 	{
 		AllocConsole();
 		HWND hWnd = GetConsoleWindow();
 		ShowWindow(hWnd, SW_SHOWNORMAL);
 	}
 #endif
-	//tako::Archetype::Create<tako::Window, tako::Bitmap>();
-	tako::World world;
-	LOG("{}", world.Create().id);
-	LOG("{}", world.Create<Velocity>().id);
-	LOG("{}", world.Create<Position>().id);
-	LOG("{}", world.Create<Position, Velocity>().id);
-	LOG("{}", world.Create<Position>().id);
 
-	for (int i = 0; i < 1000000000; i++)
-	{
-		world.Create<Position, Velocity>();
-	}
-	
-
-	auto entity = world.Create<Position>();
-	auto& pos = world.GetComponent<Position>(entity);
-	pos.pos = { 4, 2 };
-
-	LOG("{}", world.Create<Position, tako::Window>().id);
-
-	LOG("Iter Handle");
-	auto t1 = std::chrono::high_resolution_clock::now();
-	world.Iterate<Position, Velocity>([&](tako::EntityHandle handle)
-	{
-		Position& pos = world.GetComponent<Position>(handle);
-		//LOG("Iter id: {} x: {} y: {}", handle.id, pos.pos.x, pos.pos.y);
-		pos.pos.x++;
-		Velocity& vel = world.GetComponent<Velocity>(handle);
-		vel.vel.x++;
-	});
-	auto t2 = std::chrono::high_resolution_clock::now();
-	LOG("Ticks: {}", (t2 - t1).count());
-
-	LOG("Iter Comp:");
-	t1 = std::chrono::high_resolution_clock::now();
-	world.IterateComp<Position>([&](Position& pos)
-	{
-		//LOG("Iter x: {} y: {}", pos.pos.x, pos.pos.y);
-		pos.pos.x++;
-	});
-	t2 = std::chrono::high_resolution_clock::now();
-	LOG("Ticks1: {}", (t2 - t1).count());
-
-	LOG("iter created!");
-	t1 = std::chrono::high_resolution_clock::now();
-	for (auto [pos] : world.Iter<Position>())
-	{
-		//LOG("Iter x: {} y: {}", pos.pos.x, pos.pos.y);
-		pos.pos.x++;
-		//vel.vel.x++;
-	}
-	t2 = std::chrono::high_resolution_clock::now();
-	LOG("Ticks2: {}", (t2 - t1).count());
-	for (;;) {}
-	return 0;
 	tako::Window window;
 	tako::GraphicsContext context(window.GetHandle(), window.GetWidth(), window.GetHeight());
 	tako::Setup();
