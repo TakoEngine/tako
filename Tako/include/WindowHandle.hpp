3817a37a28213c2515099ebf1bb0d125e917dea3
Kevin Balz <kevin.balz@web.de>
2020-02-27 22:18:11 +0100

    include glfw

--- Tako/include/WindowHandle.hpp
+++ Tako/include/WindowHandle.hpp
@@ -5,6 +5,9 @@
 #ifdef __EMSCRIPTEN__
 #include <GLFW/glfw3.h>
 #endif
+#ifdef TAKO_GLFW
+#include <GLFW/glfw3.h>
+#endif
 
 
 
@@ -15,6 +18,9 @@ namespace tako
 #endif
 #ifdef _WIN32
 	using WindowHandle = HWND;
+#endif
+#ifdef TAKO_GLFW
+	using WindowHandle = GLFWwindow*;
 #endif
 
 	
