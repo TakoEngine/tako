6f39c0a93a60338133172402b8ba6007686fa27b
Kevin Balz <kevin.balz@web.de>
2020-12-26 00:33:52 +0100

    only in include win32 extensions in win32

--- Tako/src/platform/vulkan/VulkanContext.cpp
+++ Tako/src/platform/vulkan/VulkanContext.cpp
@@ -14,7 +14,9 @@
 #include "Math.hpp"
 #include <chrono>
 
+#ifdef  TAKO_WIN32
 static std::array<const char*, 3> vkWinExtensions = { VK_KHR_SURFACE_EXTENSION_NAME, VK_KHR_WIN32_SURFACE_EXTENSION_NAME, VK_EXT_DEBUG_UTILS_EXTENSION_NAME };
+#endif
 static std::array<const char*, 1> vkDeviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 static std::array<const char*, 1> vkWinValidationLayers = { "VK_LAYER_LUNARG_standard_validation" };
 
