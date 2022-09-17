546c671a736f2888087fa727d0ccf3ce3f9308be
Kevin Balz <kevin.balz@web.de>
2022-09-17 16:17:42 +0200

    Assets loaded relative to executable

--- Tako/include/Assets.hpp
+++ Tako/include/Assets.hpp
@@ -3,20 +3,22 @@
 
 namespace tako::Assets
 {
+	std::string GetAssetPath(const char* filePath)
+	{
+		return FileSystem::GetExecutablePath() + "/Assets" + filePath;
+	}
+
 	bool ReadAssetFile(
 		const char* filePath,
 		U8* buffer,
 		size_t bufferSize,
 		size_t& bytesRead)
 	{
-		FileSystem::GetExecutablePath();
-		auto path = std::string("./Assets") + filePath;
-		return FileSystem::ReadFile(path.c_str(), buffer, bufferSize, bytesRead);
+		return FileSystem::ReadFile(GetAssetPath(filePath).c_str(), buffer, bufferSize, bytesRead);
 	}
 
 	size_t GetAssetFileSize(const char* filePath)
 	{
-		auto path = std::string("./Assets") + filePath;
-		return FileSystem::GetFileSize(path.c_str());
+		return FileSystem::GetFileSize(GetAssetPath(filePath).c_str());
 	}
 }
