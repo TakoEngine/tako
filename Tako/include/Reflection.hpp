8e91793484b81e98906d85e0f5cc850a7f1f52ad
Kevin Balz <kevin.balz@web.de>
2025-06-11 09:38:36 +0200

    More reflection macro params

--- Tako/include/Reflection.hpp
+++ Tako/include/Reflection.hpp
@@ -11,11 +11,14 @@
 #define FE_5(WHAT, X, ...) WHAT(X),EXPAND(FE_4(WHAT, __VA_ARGS__))
 #define FE_6(WHAT, X, ...) WHAT(X),EXPAND(FE_5(WHAT, __VA_ARGS__))
 #define FE_7(WHAT, X, ...) WHAT(X),EXPAND(FE_6(WHAT, __VA_ARGS__))
+#define FE_8(WHAT, X, ...) WHAT(X),EXPAND(FE_7(WHAT, __VA_ARGS__))
+#define FE_9(WHAT, X, ...) WHAT(X),EXPAND(FE_8(WHAT, __VA_ARGS__))
+#define FE_10(WHAT, X, ...) WHAT(X),EXPAND(FE_9(WHAT, __VA_ARGS__))
 
 #define EXPAND( x ) x
-#define GET_MACRO(_0,_1,_2,_3,_4,_5,_6,_7,NAME,...) NAME
+#define GET_MACRO(_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,NAME,...) NAME
 #define FOR_EACH(action,...) \
-  EXPAND(GET_MACRO(_0,__VA_ARGS__,FE_7,FE_6,FE_5,FE_4,FE_3,FE_2,FE_1,FE_0)(action,__VA_ARGS__))
+  EXPAND(GET_MACRO(_0,__VA_ARGS__,FE_10,FE_9,FE_8,FE_7,FE_6,FE_5,FE_4,FE_3,FE_2,FE_1,FE_0)(action,__VA_ARGS__))
 
 #define REFLECT_GET_FIELD_TYPE(name) decltype(name)
 
